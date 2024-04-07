b0724f55f0e63b576010223c7f914d988a786464
Magnno <93272214+Magnno@users.noreply.github.com>
2024-04-07 20:59:34 -0300

    Added Shake Effect

--- Assets/Scripts/Editor/VFXSpriteRendererExtensionEditor.cs
+++ Assets/Scripts/Editor/VFXSpriteRendererExtensionEditor.cs
@@ -18,23 +18,47 @@ namespace PlaceHolderVFX
         {
             serializedObject.Update();
 
+            // ____Content____
+
             BeginBox("Warp");
-            SerializedProperty property = serializedObject.FindProperty("_enableWarpEffect");
-            EditorGUILayout.PropertyField(property, new GUIContent("Label"));
-            /*script.EnableWarpEffect = EditorGUILayout.Toggle("Enable", script.EnableWarpEffect);
-            if (script.EnableWarpEffect)
+            DisplayProperty("_enableWarpEffect", "Enable", out var propEnableWarp);
+            if (propEnableWarp.boolValue)
             {
-                script.WarpTilling = EditorGUILayout.Vector2Field("Tilling", script.WarpTilling);
-                script.WarpIntensity = EditorGUILayout.Vector2Field("Intensity", script.WarpIntensity);
-                script.WarpSpeed = EditorGUILayout.Vector2Field("Speed", script.WarpSpeed);
-            }*/
+                EditorGUILayout.Space(5f);
+                DisplayProperty("_warpTilling", "Tilling", out _);
+                DisplayProperty("_warpIntensity", "Intensity", out _);
+                DisplayProperty("_warpSpeed", "Speed", out _);
+            }
             EndBox();
 
-            BeginBox("Teste");
+            BeginBox("Color Correction");
+            DisplayProperty("_enableColorCorrection", "Enable", out var propEnableColor);
+            if (propEnableColor.boolValue)
+            {
+                EditorGUILayout.Space(5f);
+                DisplayProperty("_hue", "Hue", out _);
+                DisplayProperty("_saturation", "Saturation", out _);
+                DisplayProperty("_contrast", "Contrast", out _);
+                DisplayProperty("_invertColors", "Invert Colors", out _);
+            }
             EndBox();
+
+            BeginBox("Shake");
+            DisplayProperty("_enableShake", "Enable", out var propEnableShake);
+            if (propEnableShake.boolValue)
+            {
+                EditorGUILayout.Space(5f);
+                DisplayProperty("_shakeIntensity", "Intensity", out _);
+                DisplayProperty("_shakeSpeed", "Speed", out _);
+                DisplayProperty("_shakeScale", "Scale", out _);
+            }
+            EndBox();
+
+            // ____End____
 
             serializedObject.ApplyModifiedProperties();
 
+            // Button to remove component
             if (GUILayout.Button("Remove"))
             {
                 var sr = script.GetComponent<SpriteRenderer>();
@@ -59,18 +83,18 @@ namespace PlaceHolderVFX
             };
             separatorStyle.normal.background = MakeTex(600, 1, new Color(1f, 1f, 1f, 0.1f));
 
-            GUIStyle boxStyle = new GUIStyle
+            /*GUIStyle boxStyle = new GUIStyle
             {
                 margin = new RectOffset(4, 4, 10, 10),
                 padding = new RectOffset(5, 5, 5, 5),
             };
-            boxStyle.normal.background = MakeTex(600, 1, new Color(0.1f, 0.1f, 0.1f, 0.2f));
+            boxStyle.normal.background = MakeTex(600, 1, new Color(0.1f, 0.1f, 0.1f, 0.2f));*/
 
-            EditorGUILayout.BeginVertical(boxStyle);
+            EditorGUILayout.BeginVertical("Box");
             EditorGUI.indentLevel++;
 
-            EditorGUILayout.LabelField(header, titleStyle);
-            GUILayout.Box(GUIContent.none, separatorStyle);
+            EditorGUILayout.LabelField(header, titleStyle); // Header
+            GUILayout.Box(GUIContent.none, separatorStyle); // Line
         }
 
         private void EndBox()
@@ -79,6 +103,11 @@ namespace PlaceHolderVFX
             EditorGUILayout.EndVertical();
         }
 
+        private void DisplayProperty(string propertyRef, string label, out SerializedProperty property)
+        {
+            property = serializedObject.FindProperty(propertyRef);
+            EditorGUILayout.PropertyField(property, new GUIContent(label));
+        }
 
         private Texture2D MakeTex(int width, int height, Color color)
         {
--- Assets/Scripts/Runtime/VFXSpriteRendererExtension.cs
+++ Assets/Scripts/Runtime/VFXSpriteRendererExtension.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 namespace PlaceHolderVFX
 {
@@ -26,35 +27,127 @@ namespace PlaceHolderVFX
         {
             // Called when a parameter is changed in the inspector or when the scene is changed. (editor only)
             AssignMaterial();
-            UpdateParametersInMaterial();
+            UpdateAllParametersInMaterial();
         }
 
         [SerializeField] private bool _enableWarpEffect = false;
         public bool EnableWarpEffect
         {
             get { return _enableWarpEffect; }
-            set { _enableWarpEffect = value; UpdateParametersInMaterial(); }
+            set
+            {
+                _enableWarpEffect = value;
+                if (_enableWarpEffect)
+                    VFXMat.EnableKeyword("_ENABLE_WARP");
+                else
+                    VFXMat.DisableKeyword("_ENABLE_WARP");
+            }
         }
 
-        [SerializeField] private Vector2 _warpTilling = new Vector2(4f, 0f);
+        [SerializeField] private Vector2 _warpTilling = new Vector2(1f, 0f);
         public Vector2 WarpTilling
         {
             get { return _warpTilling; }
-            set { _warpTilling = value; UpdateParametersInMaterial(); }
+            set { _warpTilling = value; VFXMat.SetVector("_Warp_Tilling", _warpTilling); }
         }
 
         [SerializeField] private Vector2 _warpIntensity = new Vector2(.4f, 0f);
         public Vector2 WarpIntensity
         {
             get { return _warpIntensity; }
-            set { _warpIntensity = value; UpdateParametersInMaterial(); }
+            set { _warpIntensity = value; VFXMat.SetVector("_Warp_Intensity", _warpIntensity); }
         }
 
         [SerializeField] private Vector2 _warpSpeed = new Vector2(6f, 0f);
         public Vector2 WarpSpeed
         {
             get { return _warpSpeed; }
-            set { _warpSpeed = value; UpdateParametersInMaterial(); }
+            set { _warpSpeed = value; VFXMat.SetVector("_Warp_Speed", _warpSpeed); }
+        }
+
+        [SerializeField] private bool _enableColorCorrection = false;
+        public bool EnableColorCorrection
+        {
+            get { return _enableColorCorrection; }
+            set
+            {
+                _enableColorCorrection = value;
+                if (_enableColorCorrection)
+                    VFXMat.EnableKeyword("_ENABLE_COLOR_CORRECTION");
+                else
+                    VFXMat.DisableKeyword("_ENABLE_COLOR_CORRECTION");
+            }
+        }
+
+        [SerializeField] private bool _invertColors = false;
+        public bool InvertColors
+        {
+            get { return _invertColors; }
+            set
+            {
+                _invertColors = value;
+                if (_invertColors)
+                    VFXMat.EnableKeyword("_INVERT_COLORS");
+                else
+                    VFXMat.DisableKeyword("_INVERT_COLORS");
+            }
+        }
+
+        [SerializeField, Range(-1f, 1f)] private float _hue = 0f;
+        public float Hue
+        {
+            get { return _hue; }
+            set { _hue = value; VFXMat.SetFloat("_Hue", _hue); }
+        }
+
+        [SerializeField, Range(-1f, 1f)] private float _saturation = 0f;
+        public float Saturation
+        {
+            get { return _saturation; }
+            set { _saturation = value; VFXMat.SetFloat("_Saturation", _saturation); }
+        }
+
+        [SerializeField, Range(-1f, 1f)] private float _contrast = 0f;
+        public float Contrast
+        {
+            get { return _contrast; }
+            set { _contrast = value; VFXMat.SetFloat("_Contrast", _contrast); }
+        }
+
+
+        [SerializeField] private bool _enableShake = false;
+        public bool EnableShake
+        {
+            get { return _enableShake; }
+            set
+            {
+                _enableShake = value;
+                if (_enableShake)
+                    VFXMat.EnableKeyword("_ENABLE_SHAKE");
+                else
+                    VFXMat.DisableKeyword("_ENABLE_SHAKE");
+            }
+        }
+
+        [SerializeField] private float _shakeIntensity = 1f;
+        public float ShakeIntensity
+        {
+            get { return _shakeIntensity; }
+            set { _shakeIntensity = value; VFXMat.SetFloat("_Shake_Intensity", _shakeIntensity); }
+        }
+
+        [SerializeField] private Vector2 _shakeScale = Vector2.one;
+        public Vector2 ShakeScale
+        {
+            get { return _shakeScale; }
+            set { _shakeScale = value; VFXMat.SetVector("_Shake_Scale", _shakeScale); }
+        }
+
+        [SerializeField] private float _shakeSpeed = .2f;
+        public float ShakeSpeed
+        {
+            get { return _shakeSpeed; }
+            set { _shakeSpeed = value; VFXMat.SetFloat("_Shake_Speed", _shakeSpeed); }
         }
 
 
@@ -63,7 +156,7 @@ namespace PlaceHolderVFX
             sr.material = VFXMat;
         }
 
-        public void UpdateParametersInMaterial()
+        public void UpdateAllParametersInMaterial()
         {
             // Warp Effect
             if (_enableWarpEffect)
@@ -74,12 +167,37 @@ namespace PlaceHolderVFX
             VFXMat.SetVector("_Warp_Tilling", _warpTilling);
             VFXMat.SetVector("_Warp_Intensity", _warpIntensity);
             VFXMat.SetVector("_Warp_Speed", _warpSpeed);
+
+            // Color
+            if (_enableColorCorrection)
+                VFXMat.EnableKeyword("_ENABLE_COLOR_CORRECTION");
+            else
+                VFXMat.DisableKeyword("_ENABLE_COLOR_CORRECTION");
+
+            if (_invertColors)
+                VFXMat.EnableKeyword("_INVERT_COLORS");
+            else
+                VFXMat.DisableKeyword("_INVERT_COLORS");
+
+            VFXMat.SetFloat("_Hue", _hue);
+            VFXMat.SetFloat("_Saturation", _saturation);
+            VFXMat.SetFloat("_Contrast", _contrast);
+
+            // Shake
+            if (_enableShake)
+                VFXMat.EnableKeyword("_ENABLE_SHAKE");
+            else
+                VFXMat.DisableKeyword("_ENABLE_SHAKE");
+
+            VFXMat.SetFloat("_Shake_Intensity", _shakeIntensity);
+            VFXMat.SetVector("_Shake_Scale", _shakeScale);
+            VFXMat.SetFloat("_Shake_Speed", _shakeSpeed);
         }
 
         private void Start()
         {
             AssignMaterial();
-            UpdateParametersInMaterial();
+            UpdateAllParametersInMaterial();
         }
     }
 }
