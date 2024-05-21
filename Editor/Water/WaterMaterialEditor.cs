c84a825b832ce0881971d1ae895c9cac0998c665
Magnno <93272214+Magnno@users.noreply.github.com>
2024-05-20 22:20:09 -0300

    Water material editor upd

--- Editor/Water/WaterMaterialEditor.cs
+++ Editor/Water/WaterMaterialEditor.cs
@@ -7,8 +7,17 @@ namespace Maguinho.VFX
 {
     public sealed class WaterMaterialEditor : ShaderGUI
     {
+        private readonly Color FHColor = new(0f, 0f, 0f, .15f);
+
+        private bool FColors = false;
+        private bool FSurfaceLine = false;
+        private bool FUnderwater = false;
+        private bool FReflection = false;
+        private bool FWaves = false;
+
         public override void OnGUI(MaterialEditor editor, MaterialProperty[] properties)
         {
+            #region Methods
             void DrawLabel(string label)
             {
                 GUIStyle style = new GUIStyle();
@@ -53,9 +62,16 @@ namespace Maguinho.VFX
             {
                 EditorGUILayout.Space(space);
             }
+            #endregion
 
+            EditorUtilities.DrawBox("Rendering", () =>
+            {
+                editor.RenderQueueField();
+            });
 
-            EditorUtilities.DrawBox("Colors", () =>
+            Space(1f);
+
+            EditorUtilities.DrawFoldoutStyle1(ref FColors, "Colors", FHColor, () =>
             {
                 DrawProp("_Top_Color", "Top", out _);
                 DrawProp("_Bottom_Color", "Bottom", out _);
@@ -66,114 +82,116 @@ namespace Maguinho.VFX
 
             Space(1f);
 
-            EditorUtilities.DrawBox("Surface Line", () =>
+            EditorUtilities.DrawFoldoutStyle1(ref FSurfaceLine, "Surface Line", FHColor, () =>
             {
                 DrawProp("_Enable_Surface_Line", "Enable", out var prop);
-                if (prop.floatValue == 1f)
-                {
-                    Space();
-                    DrawProp("_Surface_Line_Color", "Color", out _);
-                    DrawProp("_Surface_Line_Length", "Length", out _);
-                }
+                EditorGUI.BeginDisabledGroup(prop.floatValue != 1f);
+                Space();
+                DrawProp("_Surface_Line_Color", "Color", out _);
+                DrawProp("_Surface_Line_Length", "Length", out _);
+                EditorGUI.EndDisabledGroup();
             });
 
             Space(1f);
 
-            EditorUtilities.DrawBox("Underwater", () =>
+            EditorUtilities.DrawFoldoutStyle1(ref FUnderwater, "Underwater", FHColor, () =>
             {
                 DrawProp("_Enable_Underwater_Render", "Enable", out var prop);
-                if (prop.floatValue == 1f)
+                EditorGUI.BeginDisabledGroup(prop.floatValue != 1f);
+
+                Space();
+                DrawProp("_Underwater_Opacity", "Opacity", out _);
+
+                Space();
+                //EditorGUILayout.LabelField("Refraction", EditorStyles.boldLabel);
+                DrawLabel("Refraction");
+                DrawProp("_Underwater_Refraction_Intensity", "Intensity", out _);
+                DrawProp("_Underwater_Refraction_Speed", "Speed", out _);
+                DrawProp("_Underwater_Refraction_Scale", "Scale", out _);
+
+                /*
+                Space();
+                EditorUtilities.DrawBox("Caustics", () =>
                 {
-                    Space();
-                    DrawProp("_Underwater_Opacity", "Opacity", out _);
-
-                    Space();
-                    //EditorGUILayout.LabelField("Refraction", EditorStyles.boldLabel);
-                    DrawLabel("Refraction");
-                    DrawProp("_Underwater_Refraction_Intensity", "Intensity", out _);
-                    DrawProp("_Underwater_Refraction_Speed", "Speed", out _);
-                    DrawProp("_Underwater_Refraction_Scale", "Scale", out _);
-
-                    /*
-                    Space();
-                    EditorUtilities.DrawBox("Caustics", () =>
+                    DrawProp("_Enable_Caustics", "Enable", out var causticsProp);
+                    if (causticsProp.floatValue == 1f)
                     {
-                        DrawProp("_Enable_Caustics", "Enable", out var causticsProp);
-                        if (causticsProp.floatValue == 1f)
-                        {
-                            Space();
-                            DrawProp("_Caustics_Texture", "Texture", out _);
-                            DrawProp("_Caustics_Tilling", "Tilling", out _);
-                            DrawProp("_Caustics_Intensity", "Intensity", out _);
-                            DrawProp("_Caustics_Speed", "Speed", out _);
-                            DrawProp("_Caustics_Power", "Fading", out _);
-                            DrawProp("_Caustics_Distortion_Intensity", "Distortion Intensity", out _);
-                            DrawProp("_Caustics_Distortion_Scale", "Distortion Scale", out _);
-                            DrawProp("_Caustics_Distortion_Speed", "Distortion Speed", out _);
-                        }
-                    }, indentLvl: 0);
-                    */
-                }
+                        Space();
+                        DrawProp("_Caustics_Texture", "Texture", out _);
+                        DrawProp("_Caustics_Tilling", "Tilling", out _);
+                        DrawProp("_Caustics_Intensity", "Intensity", out _);
+                        DrawProp("_Caustics_Speed", "Speed", out _);
+                        DrawProp("_Caustics_Power", "Fading", out _);
+                        DrawProp("_Caustics_Distortion_Intensity", "Distortion Intensity", out _);
+                        DrawProp("_Caustics_Distortion_Scale", "Distortion Scale", out _);
+                        DrawProp("_Caustics_Distortion_Speed", "Distortion Speed", out _);
+                    }
+                }, indentLvl: 0);
+                */
+
+                EditorGUI.EndDisabledGroup();
             });
 
             Space(1f);
 
-            EditorUtilities.DrawBox("Reflection", () =>
+            EditorUtilities.DrawFoldoutStyle1(ref FReflection, "Reflection", FHColor, () =>
             {
                 DrawProp("_Enable_Reflection", "Enable", out var prop);
-                if (prop.floatValue == 1f)
-                {
-                    Space();
-                    DrawProp("_Reflection_Opacity", "Opacity", out _);
-                    DrawProp("_Reflection_Norm_Height", "Height", out _);
-                    DrawProp("_Reflection_Fading", "Fading", out _);
-                    DrawProp("_Reflection_Scale", "Scale", out _);
+                EditorGUI.BeginDisabledGroup(prop.floatValue != 1f);
 
-                    Space();
-                    DrawLabel("Warp");
-                    EditorGUILayout.LabelField("X Axis", EditorStyles.boldLabel);
-                    DrawProp("_Reflection_X_Warp_Intensity", "Intensity", out _);
-                    DrawProp("_Reflection_X_Warp_Frequency", "Frequency", out _);
-                    DrawProp("_Reflection_X_Warp_Speed", "Speed", out _);
+                Space();
+                DrawProp("_Reflection_Opacity", "Opacity", out _);
+                DrawProp("_Reflection_Norm_Height", "Height", out _);
+                DrawProp("_Reflection_Fading", "Fading", out _);
+                DrawProp("_Reflection_Scale", "Scale", out _);
 
-                    Space();
-                    EditorGUILayout.LabelField("Y Axis", EditorStyles.boldLabel);
-                    DrawProp("_Reflection_Y_Warp_Intensity", "Intensity", out _);
-                    DrawProp("_Reflection_Y_Warp_Frequency", "Frequency", out _);
-                    DrawProp("_Reflection_Y_Warp_X_Multiplier", "X Speed Multiplier", out _);
-                    DrawVector2Prop("_Reflection_Y_Warp_Speed", "Speed");
-                }
+                Space();
+                DrawLabel("Warp");
+                EditorGUILayout.LabelField("X Axis", EditorStyles.boldLabel);
+                DrawProp("_Reflection_X_Warp_Intensity", "Intensity", out _);
+                DrawProp("_Reflection_X_Warp_Frequency", "Frequency", out _);
+                DrawProp("_Reflection_X_Warp_Speed", "Speed", out _);
+
+                Space();
+                EditorGUILayout.LabelField("Y Axis", EditorStyles.boldLabel);
+                DrawProp("_Reflection_Y_Warp_Intensity", "Intensity", out _);
+                DrawProp("_Reflection_Y_Warp_Frequency", "Frequency", out _);
+                DrawProp("_Reflection_Y_Warp_X_Multiplier", "X Speed Multiplier", out _);
+                DrawVector2Prop("_Reflection_Y_Warp_Speed", "Speed");
+
+                EditorGUI.EndDisabledGroup();
             });
 
             Space(1f);
 
-            EditorUtilities.DrawBox("Waves", () =>
+            EditorUtilities.DrawFoldoutStyle1(ref FWaves, "Waves", FHColor, () =>
             {
                 DrawProp("_Enable_Waves", "Enable", out var prop);
-                if (prop.floatValue == 1f)
-                {
-                    Space();
-                    DrawProp("_Global_Amplitude", "Global Amplitude", out _);
-                    DrawProp("_Global_Speed", "Global Speed", out _);
+                EditorGUI.BeginDisabledGroup(prop.floatValue != 1f);
 
-                    Space();
-                    DrawWave(1);
+                Space();
+                DrawProp("_Global_Amplitude", "Global Amplitude", out _);
+                DrawProp("_Global_Speed", "Global Speed", out _);
 
-                    Space();
-                    DrawWave(2);
+                Space();
+                DrawWave(1);
 
-                    Space();
-                    DrawWave(3);
+                Space();
+                DrawWave(2);
 
-                    Space();
-                    DrawWave(4);
+                Space();
+                DrawWave(3);
 
-                    Space();
-                    DrawWave(5);
+                Space();
+                DrawWave(4);
 
-                    Space();
-                    DrawWave(6);
-                }
+                Space();
+                DrawWave(5);
+
+                Space();
+                DrawWave(6);
+
+                EditorGUI.EndDisabledGroup();
             });
 
             //base.OnGUI(editor, properties);
